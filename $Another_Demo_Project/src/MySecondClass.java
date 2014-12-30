982ea668db581449a7e4daa77f64223a730aa9ce
kabuman <kabuman@posteo.de>
2014-12-30 06:57:58 +0100

    new file

---
+++ $Another_Demo_Project/src/MySecondClass.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+
+/**
+ * @author Karsten Buchmann
+ *
+ */
+public class MySecondClass {
+
+	/**
+	 * 
+	 */
+	public MySecondClass() {
+		// TODO Auto-generated constructor stub
+	}
+
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
